4c2b2c257941066d59e8b49a54abec6d25b06b20
ahaoboy <504595380@qq.com>
2025-02-28 15:51:12 +0800

    add build.sh

--- build.sh
+++ build.sh
@@ -7,13 +7,19 @@ fi
 
 TARGET=$1
 
+git clone https://github.com/quickjs-ng/quickjs.git quickjs-ng --depth=1
+cd quickjs-ng
+
 cmake -S . -B build
 cmake --build build
-mkdir dist
 
-cp -r ./build/*qjs* dist
+mkdir ../dist
+
+cp -r ./build/*qjs* ../dist
+
+cd ..
 
 ls -lh dist
 
-tar -czf ./qjs-${TARGET}.tar.gz -C ./dist .
+tar -czf ./qjs-${TARGET}.tar.gz -C dist .
 ls -l ./qjs-${TARGET}.tar.gz
